cb4d48e62786792a81a08c9e739aa62fd52daa2f
MaleehaTanveer-dbug <maleehaxha@gmail.com>
2025-08-15 20:38:26 +0500

    create our first python file

---
+++ python.py
@@ -0,0 +1,10 @@
+customer={
+    "firstName":"Hans",
+    "lastName":"smith",
+    "age": 30,
+    "gender":"male",
+}
+print(customer["firstName"])
+print(customer["lastName"])
+print(customer["age"])
+print(customer["gender"])
